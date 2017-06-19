490b74062169a82e7e2dac8bb91da17468a49106
Greg-R <soapy-smith@moose.com>
2017-06-18 22:14:02 -0400

    Debugging

--- software/stepper-meter1.js
+++ software/stepper-meter1.js
@@ -27,8 +27,8 @@ setGpio('P9.10');
 
 function step() {
     
-    setTimeout(exec(`echo 0 > /sys/class/gpio/gpio67/value`), 100);
-    setTimeout(exec(`echo 1 > /sys/class/gpio/gpio67/value`), 100);
+    setTimeout(() => {exec(`echo 0 > /sys/class/gpio/gpio67/value`)}, 100);
+    setTimeout(() => {exec(`echo 1 > /sys/class/gpio/gpio67/value`)}, 100);
 }
 
 for(let i = 0; i < 100; i++) {
